8cf0e99f3cec84e9ce6e5a27ed4a3e131159ca78
Beatriz Brito <110315709+bea-brito@users.noreply.github.com>
2023-06-01 20:04:17 -0300

    Update SOMATORIO.test.js

--- SOMATORIO.test.js
+++ SOMATORIO.test.js
@@ -2,5 +2,8 @@ const somatorio = require("./somatorio");
 const numeros = [3,2,7];
 test("somatorio",()=>{
     expect(somatorio (numeros)).toBe(12);
+})
+
+test("somatorio",()=>{
     expect(somatorio (numeros)).not.toBe(11);
 })
