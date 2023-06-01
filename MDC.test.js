3ac0da17e86e3afa4e59cc88837366f8079175f8
Beatriz Brito <110315709+bea-brito@users.noreply.github.com>
2023-06-01 20:02:53 -0300

    Update MDC.test.js

--- MDC.test.js
+++ MDC.test.js
@@ -4,3 +4,8 @@ test("MDC",()=>{
     expect(MDC(2,1)).toBe(1);
     expect(MDC(2,1)).not.toBeLessThan(0);
 })
+
+test("MDC",()=>{
+    expect(MDC(20,10)).toBe(10);
+    expect(MDC(20,10)).not.toBeLessThan(10);
+})
